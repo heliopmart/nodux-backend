0677d84b4a51a3cde08ad49689b44518d4440351
Hélio Martins <helioperesmartinsneto@gmail.com>
2026-02-26 22:45:23 -0300

    chore(config): setup main application entry point and spring properties

---
+++ src/main/java/com/nodux/NoduxApplication.java
@@ -0,0 +1,14 @@
+package com.nodux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Ponto de entrada principal do ecossistema Nodux.
+ */
+@SpringBootApplication
+public class NoduxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NoduxApplication.class, args);
+    }
+}
